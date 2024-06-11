71e679be650dcaa062a98ba443dee4c0755de0bf
Aniket Sable <aniketsable0101@gmail.com>
2024-06-11 05:16:51 +0000

    Initial Change

--- prac_demo/srv/demo_srv.js
+++ prac_demo/srv/demo_srv.js
@@ -1,6 +1,6 @@
 const cds = require('@sap/cds')
 const dbClass = require("sap-hdbext-promisfied")
-const hdbext = require("@sap/hdbext")
+// const hdbext = require("@sap/hdbext")
 
 module.exports = cds.service.impl(function () {
     
